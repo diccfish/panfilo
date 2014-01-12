66ee031db6a6241ccfa1dcd200258066bce89370
diccfish <diccfish@gmail.com>
2014-01-12 13:21:37 -0800

    panfilo de narvaez story for floriduh.org

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php include('header.php'); ?>
+	<main>
+		
+		<article class="narvaez">
+			<div>
+				<h1>Panfilo de Narvaez</h1>
+				<h2>was an asshole</h2>
+			</div>
+		</article>
+		
+		<article class="boca">
+			<section>
+				<div class="land"></div>
+				<div class="water"></div>
+				<div class="boat"></div>
+				<div class="sail"></div>
+				<div class="teepee"></div>
+				<div class="teepee2"></div>
+				<div class="teepee3"></div>
+				<div class="teepee4"></div>
+				<div class="explain">
+					<h1>Narvaez Set Sail</h1>
+					<p>from Cuba on March 4th, 1528, with 5 ships, 400 men, 12 women, 80 horses, and a pack of greyhounds. That was definetley the high point of his trip.</p>
+					<p>Narvaez's ship was tossed off course by a hurricane, and ended up in Boca Ciega Bay. de Narvaez and his men went ashore. On shore he found a Tocobaga village</p>
+				</div>
+			</section>
+		</article>
+
+		<article class="tocobaga-container">
+			<div class="tocobaga"></div>
+			<div>
+				<h1>Gold</h1>
+				<p>All Tocobaga had fled the village, and Narvaez searched all homes. He found a gold rattle!! GOOOLLLDD! Narvaez head inland to search for more. He found the Tocobaga chief. The chief showed Narvaez the dead Spanish bodies that had been washed ashore during the storm. The Tocobaga had wrapped the dead in deer skin, which offended the christian Narvaez. Narvaez chopped off the chief's nose and feed the chief's mother to his greyhounds.</p>
+				<h2>WTF</h2>  
+			</div>
+		</article>
+
+		<article class="journey">
+			<div>
+				<h1>Narvaez Was Not Finished</h1>
+				<p>In fact no one in his party would be finished for a long long time.</p>
+				<p>As Narvaez's party moved northward, into present day tallahassee, they ran into the apalachee tribe. After Narvaez insulted the apalachee chief and trying to kill the tribe, he was attacked by the apalachee.</p>
+				<p>Narvaez and his party made the decision to build rafts and sail to mexico. Along the way Narvaez died, probably drowned. 8 years later, 4 people, including alvar nunez cabeza de vaca, emerged in Mexico</p> 
+			</div>
+		</article>
+
+		<article>
+			<div>
+				<h1>Alvar Nunez Cabeza de Vaca</h1>
+				<p>Had a long way to go, but that is a whole tale for another time, unless you are <a href="http://en.wikipedia.org/wiki/%C3%81lvar_N%C3%BA%C3%B1ez_Cabeza_de_Vaca" target="_blank">craving more</a></p>
+			</div>
+		</article>
+
+		<article>
+			<div>
+				<h1>A Lesson From Our Teacher</h1>
+				<p>Click to play</p>
+				<iframe width="420" height="315" src="//www.youtube.com/embed/ogXLMj3UuLQ?rel=0" frameborder="0" allowfullscreen></iframe>
+		</article>
+
+		<article class="links">
+			<div>
+				<h1>Learn More About Panfilo de Narvaez</h1>
+				<a href="http://www.tampabay.com/features/humaninterest/who-in-the-world-is-panfilo-de-narvaez-mdash-and-why-should-we-care/459922" target="_blank">Tampa Bay Times</a>
+				<a href="http://en.wikipedia.org/wiki/Tocobaga" target="_blank">Tocobaga</a>
+			</div>
+		</article>
+
+		<article class="home">
+			<div>
+				<a href="/"><h1>Back Home</h1></a>
+			</div>
+		</article>
+	</main>
+<?php include('footer.php'); ?>
